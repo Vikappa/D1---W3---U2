56bec7a1dc46558678b4c7cee0d3bc22ef0d9173
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-15 15:59:37 +0100

    Fine

--- esercizi.js
+++ esercizi.js
@@ -31,18 +31,56 @@ darioBianchi.confrontaEtàCon(marcoNeri)
 console.log("") //spaziatura per i risultati del prossimo esercizio
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
+class Pet {
+    constructor(petName, petOwner, specie, razza) {
+        this.petName = petName
+        this.petOwner = petOwner
+        this.specie = specie
+        this.razza = razza
+    }
+    stringaPet() {
+        return `${this.petName}, ${this.specie} di ${this.petOwner}, ${this.razza}`
+    }
+}
+
 const listaPetSchermo = document.getElementById('listaPetSchermo')
 let listaPetAcquisiti = []
-
-const updatePetList = function () {
+const form = document.getElementById('petForm')
+const updatePetListSchermo = function () {
     listaPetSchermo.innerHTML = ``
 }
 
 const addPet = function () {
-    updatePetList()
+    // I controlli che i campi siano riempiti vengono effettuati dal form
+    let petName = document.getElementById('petName').value
+    let petOwner = document.getElementById('petOwner').value
+    let specie = document.getElementById('specie').value
+    let razza = document.getElementById('razza').value
+
+    let newpet = new Pet(petName, petOwner, specie, razza)
+
+    let presente = false
+
+    for (let i = 0; i < listaPetAcquisiti.length; i++) {
+        if (listaPetAcquisiti[i].stringaPet === newpet.stringaPet) {
+            presente === true
+        }
+    }
+
+    if (!presente) {
+        listaPetAcquisiti.push(newpet)
+    }
 
 
+    for (let index = 0; index < listaPetAcquisiti.length; index++) {
+        console.log(listaPetAcquisiti[index].stringaPet())
+    }
 
-    let pRitorno
 }
 
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault() // fermiamo la pagina dal refresh
+    addPet()
+    updatePetListSchermo()
+})
